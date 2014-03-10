3cb308cfb0a36ed6cc15483ce0c77cd484dabe26
OpenShift guest <gauthier.montavon@gmail.com>
2014-03-10 16:00:48 -0400

    first call to ffmpeg extension

--- site/index.php
+++ site/index.php
@@ -4,7 +4,7 @@ echo 'Hello gomodev.org';
 extension_loaded('ffmpeg') or die('Error in loading ffmpeg');
 
 $ffmpegInstance = new ffmpeg_movie('media_samples/cbw3.avi');
-echo "getDuration: " . $ffmpegInstance-&gt;getDuration() .
+echo "getDuration: " . $ffmpegInstance->getDuration();
 
 
 require("html_templates/footer.php"); ?> 
