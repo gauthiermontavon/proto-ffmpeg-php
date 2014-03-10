6d9999795dfba1cb518d26735e2385ad92390672
OpenShift guest <gauthier.montavon@gmail.com>
2014-03-10 15:30:40 -0400

    header and footer as included parts

---
+++ site/html_templates/header.php
@@ -0,0 +1,9 @@
+<!doctype html>   
+<head>   
+<meta charset="utf-8">
+<title>ffmpeg php - Prototype project</title>
+<!-- meta description must be limited to 160 characters or less -->
+<meta name="description" content="This is a page to do some tricks about web technologies"> 
+<meta name="author" content="Gauthier Montavon">
+</head>
+<body>
--- site/index.php
+++ site/index.php
@@ -1,4 +1,4 @@
-<?php
-    echo 'Hello gomodev.org';
+<?php require("html_templates/header.php");
+echo 'Hello gomodev.org';
 
-?>
+<?php require("html_templates/footer.php"); ?> 
