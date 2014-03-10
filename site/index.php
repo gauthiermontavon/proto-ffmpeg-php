d93959ef8ac40e457f1c5e87adf2ec37e824e449
OpenShift guest <gauthier.montavon@gmail.com>
2014-03-10 15:52:47 -0400

    check extension ffmeg loaded

--- site/index.php
+++ site/index.php
@@ -1,3 +1,10 @@
 <?php require("html_templates/header.php");
 echo 'Hello gomodev.org';
+
+extension_loaded('ffmpeg') or die('Error in loading ffmpeg');
+
+//$ffmpegInstance = new ffmpeg_movie('clock.avi');
+//echo "getDuration: " . $ffmpegInstance-&gt;getDuration() .
+
+
 require("html_templates/footer.php"); ?> 
