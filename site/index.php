5f865e19ca95748fe86f59b247e5c5248c813802
OpenShift guest <gauthier.montavon@gmail.com>
2014-03-11 07:10:52 -0400

    fffmpeg command line

--- site/index.php
+++ site/index.php
@@ -1,5 +1,5 @@
 <?php require("html_templates/header.php");
-echo 'Hello gomodev.org';
+echo 'Hello gomodev.org !';
 
 extension_loaded('ffmpeg') or die('Error in loading ffmpeg');
 
@@ -7,7 +7,6 @@ $ffmpegInstance = new ffmpeg_movie('media_samples/cbw3.avi');
 echo "getDuration: " . $ffmpegInstance->getDuration();
 
 
-exec('ffmpeg -f image2 -r 1/5 -i media_samples/image%03d.png -vcodec libx264 media_samples/out.mp4');
-
+exec('ffmpeg -f image2 -r 1/5 -i media_samples/image%03d.png media_samples/out.mpg');
 
 require("html_templates/footer.php"); ?> 
